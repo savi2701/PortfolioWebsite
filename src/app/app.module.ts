d398caacf4679a80c479135254ec513cd693bd26
savi2701 <75682948+savi2701@users.noreply.github.com>
2021-06-12 21:34:10 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovingEyesDirective } from './moving-eyes.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovingEyesDirective
   ],
   imports: [
     BrowserModule,
