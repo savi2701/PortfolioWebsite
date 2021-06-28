79f10c3698bf5a32a925e94398816ab315316820
savi2701 <75682948+savi2701@users.noreply.github.com>
2021-06-28 22:01:19 +0530

    routing done

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-detail',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
+  projectDetail = [
+    {
+      heading: "Portfolio Website",
+      subheading: "My portfolio website to showcase my work and skills.",
+      aim : {
+        heading: "The aim was to create a portfolio website from scratch, without the use of any external library like Bootstrap or Tailwind. The entire project is created using Angular, HTML and SCSS. All the illustrations are used from undraw.co.",
+        subheading: "The design inspiration was taken from the portfolio website of Sean Halpin."
+      }
+      
+    },
+    {
+      heading: "Portfolio Website2",
+      subheading: "My portfolio website to showcase my work and skills.",
+      aim : {
+        heading: "The aim was to create a portfolio website from scratch, without the use of any external library like Bootstrap or Tailwind. The entire project is created using Angular, HTML and SCSS. All the illustrations are used from undraw.co.",
+        subheading: "The design inspiration was taken from the portfolio website of Sean Halpin."
+      
+      }
+    }
+  ]
+  selectedProject: any;
+  id!: number;
   ngOnInit(): void {
+    this.id = +this.route.snapshot.params['id'];
+    this.selectedProject = this.projectDetail[this.id - 1];
   }
 
 }
